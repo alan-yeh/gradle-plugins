a7f456dfdd119f98948a907861c4efc1138fd0e6
Alan Yeh <alan@yerl.cn>
2016-12-20 22:12:57 +0800

    Update

--- android-config-plugin/src/main/groovy/cn/yerl/gradle/plugin/AndroidConfigPlugin.groovy
+++ android-config-plugin/src/main/groovy/cn/yerl/gradle/plugin/AndroidConfigPlugin.groovy
@@ -4,18 +4,19 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 /**
+ * 统一设置Android的编译、包版本等
  * Created by alan on 2016/11/1.
  */
 class AndroidConfigPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.ext["compileSdkVersion"] = 23;
-        project.ext["buildToolsVersion"] = "23.0.3"
+        project.ext["buildToolsVersion"] = "23.0.3";
 
         project.ext["minSdkVersion"] = 17;
         project.ext["targetSdkVersion"] = 23;
 
-        project.ext["supportVersion"] = "23.0.3";
+        project.ext["supportVersion"] = "23.4.0";
     }
 
 }
